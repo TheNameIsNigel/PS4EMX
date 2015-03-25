6225b38c1c7db1ecb18e5135760291baa27f5fb6
Brenda <fudgeshaker@inbox.com>
2015-03-24 23:36:09 -0400

    working on Linux GUI with Gtkmm

--- GUI/MainGUI.cpp
+++ GUI/MainGUI.cpp
@@ -1,67 +1,18 @@
-#include <windows.h>
-#include "/Firmware/FlashBlobs.hpp"
-// Will change to another GUI system later
-#define IDM_MYMENURESOURCE   3
-int APIENTRY WinMain(HINSTANCE hThis, HINSTANCE hPrev, LPSTR cmd, int Show)
+#include <cstdint>
+#include <gtkmm.h>
+typedef uint64_t uQUAD;
+typedef uint32_t uDOUBLE;
+typedef uint16_t uWORD;
+typedef uint8_t uBYTE;
+typedef int64_t sQUAD;
+typedef int32_t sDOUBLE;
+typedef int16_t sWORD;
+typedef int8_t sBYTE;
+int main(int args, char *argv[])
 {
-HWND HANDLE;
-MSG MESSAGE;
-const char *CLASSNAME = "MainWindow";
-const char *WINDOW_TITLE[] = "PS4EMX";
-bool Loop = 1;
-WNDCLASS Wclass;
-MSG msg;
-Wclass.cbSize = sizeof(WNDCLASS);
-Wclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
-Wclass.lpfnWndProc = (WNDPROC)WndProc;
-Wclass.cbClsExtra = 0;
-Wclass.cbWndExtra = 0;
-Wclass.hIcon = LoadIcon(NULL, IDI_APPLICATION); // Easily changeable later
-Wclass.hCursor = LoadCursor(NULL, IDC_ARROW); // Same as above
-Wclass.hbrBackground = NULL;
-Wclass.lpszClassName = CLASSNAME;
-Wclass.lpszMenuName = MAKEINTRESOURCE(IDM_MYMENURESOUCE);
-if(!RegisterClass(&Wclass))
-{
-return 0;
-}
-if (!(HANDLE = CreateWindowEx(WS_EX_APPWINDOW | WS_EX_WINDOWEDGE, clsName, title, WS_OVERLAPPEDWINDOW | WS_CLIPSIBLINGS
-| WS_CLIPCHILDREN, CW_USEDEFAULT, CW_USEDEFAULT, 800, 640, NULL, NULL, hThis, NULL)))
-     {    
-       return 0;  
-     }  
-if(HANDLE == NULL)
-{
-  return FALSE;
-}
-ShowWindow(HANDLE, Show);
-UpdateWindow(HANDLE);
-while(Loop)
-{
-  TranslateMessage(&MESSAGE);
-  DispatchMessage(&MESSAGE);
-}
-return MESSAGE.wParam;
-}
-LRESULT CALLBACK WINDOW(HWND hwnd, unsigned int message, WPARAM wparam, LPARAM lparam)
-{
-switch(message)
-{
-case 
-WM_COMMAND:
-switch(LOWORD(wParam))
-{
-case
-IDM_FI_OPEN: break; // Needs toggle specific function
-}
-case
-WM_PAINT: break;
-// This won't be used for GPU stuff
-case
-WM_CLOSE: break;
-// All cleanup goes here
-default:
-return DefWindowProc(hwnd, message, wparam, lparam);
-}
-return NULL;
+  Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(args, argv, "org.gtkmm.examples.base");
+  Gtk::Window MAIN;
+  MAIN.set_default_size(640, 480);
+
+  return app->run(window);
 }
